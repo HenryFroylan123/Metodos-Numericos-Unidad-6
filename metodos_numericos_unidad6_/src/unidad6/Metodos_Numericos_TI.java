ea899df52f8f71700baeb23ac9dbc243689c7763
Henry-Froylan-Chuc-Tec <56937480+HenryFroylan123@users.noreply.github.com>
2020-06-03 22:13:27 -0500

    Add files via upload
    Integrantes:
    
    -Henry Froylan Chuc Tec 
    -Jose Natividad Tun Tun 
    4°A

---
+++ metodos_numericos_unidad6_/src/unidad6/Metodos_Numericos_TI.java
@@ -0,0 +1,13 @@
+package unidad6;
+
+
+public class Metodos_Numericos_TI {
+
+    public static void main(String[] args) {
+    dlgEuler PrimerPaso = new dlgEuler();
+        PrimerPaso.setVisible(true);
+        
+    PrimerPaso.setLocationRelativeTo(null);
+    }
+    
+}
